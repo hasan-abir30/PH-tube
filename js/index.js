7ecb4a5b22d2affdc568a8e7927ef18884d38a07
hasan-abir30 <hasibulhasanabir7@gmail.com>
2024-10-06 11:15:05 +0600

    add category button

--- js/index.js
+++ js/index.js
@@ -1,1 +1,24 @@
-console.log("hasan");
+// get the category data using fetch
+
+function loadData(){
+    fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
+        .then(res => res.json())
+        .then(data => displayData(data.categories))
+        .catch(err => console.log(err));
+}
+function displayData(data){
+    const div = document.getElementById('button-position');
+  data.forEach((item)=>{
+    const categories = item.category;
+    const button = document.createElement('button');
+    // console.log(button);
+    button.innerText=categories;
+    button.classList='btn btn-primary';
+    // console.log(button);
+    div.appendChild(button);
+    div.style.marginTop='30px'
+  })
+}
+
+loadData();
+
